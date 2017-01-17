c6c7b8aa411a1fc4d21e803b24c496cf841ec8ae
Xavier Garcia Buils <xgbuils@gmail.com>
2017-01-17 23:19:42 +0100

    remove unnecessary .apply uses

--- src/constructors/cartesian.js
+++ src/constructors/cartesian.js
@@ -9,14 +9,14 @@ function Cartesian (validator, Iterum) {
             var generators = args.map(function (list) {
                 return function (...params) {
                     var _ = params[params.length - 1]
-                    _.apply(null, params)
+                    _(...params)
                     return IterumList(list).build()()
                 }
             })
             generators.push(function (...params) {
                 return IterumList([params.slice(0, -1)]).build()()
             })
-            var product = compose.apply(null, generators)
+            var product = compose(...generators)
             return product()
         })
     }
