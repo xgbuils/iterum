482b43a8dd9a2c2e75f8f479b26c7749b95b793b
Xavier Garcia Buils <xgbuils@gmail.com>
2017-06-11 17:33:15 +0200

    add call slice twice performance test #18

--- perf/slice/index.js
+++ perf/slice/index.js
@@ -2,6 +2,7 @@ const arraySmallerStartSuite = require('./array_smaller_start')
 const arrayBiggerStartSuite = require('./array_bigger_start')
 const noIndexableSmallerStartSuite = require('./no_indexable_smaller_start')
 const noIndexableBiggerStartSuite = require('./no_indexable_bigger_start')
+const twoCallsSuite = require('./two_calls')
 
 module.exports = [{
     name: 'array - small start index',
@@ -15,4 +16,7 @@ module.exports = [{
 }, {
     name: 'no indexable iterable - big start index',
     suite: noIndexableBiggerStartSuite
+}, {
+    name: 'two calls',
+    suite: twoCallsSuite
 }]
